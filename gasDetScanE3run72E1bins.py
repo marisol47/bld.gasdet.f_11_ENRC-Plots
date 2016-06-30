14b74377c216d43ca7fcab15642ca161bbcbc25e
marisol47 <mguzman@g.hmc.edu>
2016-06-30 13:06:06 -0700

    Update gasDetScanE3run72E1bins.py

--- gasDetScanE3run72E1bins.py
+++ gasDetScanE3run72E1bins.py
@@ -13,5 +13,40 @@ idx3e2run72 = np.logical_and(e2Bounds, idx3)
 w = h5['acq.waveforms']
 e1pos = h5['regr_predict_acq.e1.pos'][:]
 
-noNegNums = np.array(filter(lambda x: x > -1, e1pos)) #Filtering all of the nega
+noNegNums = np.array(filter(lambda x: x > -1, e1pos)) #Filtering all of the negative numbers out
+minNum = min(noNegNums)
+maxNum = max(noNegNums)
+minNum = math.floor(minNum*10)/10                                                                                           
+maxNum = math.ceil(maxNum*10)/10
+binBounds = np.linspace(minNum, maxNum, 21)
+all_bins_wavs = []
+all_bins_idx = []
+gasDetectionData = h5['bld.gasdet.f_11_ENRC'][:] # all of the gas detection values
+medianGDD = np.median(gasDetectionData)
+
+gasinterval = gasDetectionData >= medianGDD #analyzing all of the point bigger than or equal to the gas detection data
+
+
+for i in range(len(binBounds)-1): 
+	e1bin = np.logical_and(e1pos >= binBounds[i], e1pos < binBounds[i+1])
+	boolbin = np.logical_and(e1bin, idx3e2run72)
+	boolbin = np.logical_and(boolbin, gasinterval)
+	if sum(boolbin) >20: # checks that there are atleast 20 items on the array before creating a bin to plot
+		ch0_e1_bin = w[boolbin, 0, :]
+		bin_mean  = np.mean(ch0_e1_bin, axis = 0)
+		all_bins_wavs += [bin_mean]
+		all_bins_idx += [i]
+
+
+mean = np.mean(all_bins_wavs, axis = 0)
+plt.plot(mean, label = "mean of plots")
+
+
+
+for idx,wv in zip(all_bins_idx ,all_bins_wavs): # a way to plot a list of arrays more efficiently
+	
+	plt.plot(wv, label = "bin %f " %binBounds[idx])
+	
+plt.legend()	
+plt.show()
 
